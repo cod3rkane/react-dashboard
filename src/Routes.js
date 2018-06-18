7a1dabd0053bfdf189e8c7e56fbe1abd204138e5
cod3rkane <jcesar.developer@gmail.com>
2018-06-18 12:30:19 -0300

    Login Page

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import App from './containers/home/App';
+import { Login } from './containers/login/Login';
 
 const NoMatch = () => {
   return (
@@ -15,6 +16,7 @@ export const Routes = () => {
   return (
     <Switch>
       <Route exact path="/" component={App} />
+      <Route path="/login" component={Login} />
       <Route component={NoMatch}/>
     </Switch>
   );
---
+++ src/containers/login/Login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Login = (args) => {
+  console.log({ args });
+  return (
+    <div>
+      Login Page
+    </div>
+  );
+};
